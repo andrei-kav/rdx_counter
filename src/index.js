5678ca5bbf5908c31cc3db53fdb2e3dd313eb0b6
Andrei kav <kav@example.com>
2020-01-07 20:34:47 +0300

    added btns downloadNumber, uploadNumber; connected to db.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { createStore } from "redux";
 
+const url = 'http://localhost:3001/numbers';
+
 const reducer = (state = 0, action) => {
     switch (action.type) {
         case  'INC':
-            return state + 1;
+            return +state + 1;
         case 'DEC':
             return state - 1;
         case 'RST':
-            console.log(action);
             return state = 0;
+        case 'DWNLD':
+            return state = action.value;
         default:
             return state;
     }
@@ -16,7 +19,45 @@ const reducer = (state = 0, action) => {
 
 const inc = () => ({type: 'INC'});
 const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RST'});
+const rnd = () => ({type: 'RST'});
+const dwnld = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw {
+            status: response.status,
+            text: new Error(`Could not fetch ${url}, received ${response.status}`)
+        }
+    }
+    const data = await response.json();
+    const id = Math.floor(Math.random()*data.length + 1);
+    console.log(id);
+    const value = data.filter(number => number.id === id).map(number => number.const || number.saved);
+    return {
+        type: 'DWNLD',
+        value
+    }
+};
+
+const upld = async (url) => {
+    const saved = document.getElementById('counter').textContent;
+    if (+saved === 0) return console.error('counter can not be 0');
+    const obj = {
+        saved
+    };
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(obj)
+    });
+    if (!response.ok) {
+        throw {
+            status: response.status,
+            text: new Error(`Could not fetch ${url}, received ${response.status}`)
+        }
+    }
+};
 
 const store = createStore(reducer);
 
@@ -28,6 +69,12 @@ document.getElementById('dec').addEventListener('click', () => {
 });
 document.getElementById('rst').addEventListener('click', () => {
     store.dispatch(rnd());
+});
+document.getElementById('dwnld').addEventListener('click',async () => {
+    store.dispatch(await dwnld(url));
+});
+document.getElementById('upld').addEventListener('click',  () => {
+     upld(url);
 });
 
 const update = () => {
